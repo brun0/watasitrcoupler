b7c4339d0c16935097c188ba1cbdf8a26673e471
Julien Veyssier <eneiluj@posteo.net>
2020-03-31 17:00:34 +0200

    code formatting of Rcoupler
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- Rcoupler.R
+++ Rcoupler.R
@@ -49,21 +49,24 @@ year_sim <- 2017
 cormas_sim_day_nb <- 4
 
 ####### 2.2 Importation of meteo data input  #######
-input_meteo      = read.csv(file.path(wd, 'climatefile/climate_buech_2017.csv'), header=TRUE, sep=",", dec=".", stringsAsFactors=FALSE)
-meteo      = input_meteo[which(input_meteo$year == year_sim),] ; str(meteo)
+input_meteo = read.csv(file.path(wd, 'climatefile/climate_buech_2017.csv'), header=TRUE, sep=",", dec=".", stringsAsFactors=FALSE)
+meteo = input_meteo[which(input_meteo$year == year_sim),]
+str(meteo)
 
 ####### 2.3 Generation of an Optirrig paramfile for each WatASit plots  #######
 list_idParcel <- NULL
 if (with_optirrig) {
-  list_idParcel <- optiParams(paste0(wd,'paramfiles/'),
-                              case_study_name,
-                              'watasit.csv',
-                              'paramDB.csv',
-                              'climate_buech_2017.csv',
-                              year_sim,
-                              1,
-                              365,
-                              'irrig_file_watasit.dat')
+  list_idParcel <- optiParams(
+    paste0(wd,'paramfiles/'),
+    case_study_name,
+    'watasit.csv',
+    'paramDB.csv',
+    'climate_buech_2017.csv',
+    year_sim,
+    1,
+    365,
+    'irrig_file_watasit.dat'
+  )
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -100,18 +103,18 @@ r <- initSimu()
 ####### 4. Initialization of Optirrig model #######
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 if (with_optirrig) {
-#param_frame <- data.frame(); irr <- data.frame()
-#for (i in 1:length(list_idParcel)){
-#  ####### 4.1 Load params of each plot #######
-#  param = read.csv(paste0(wd,'paramfiles/paramfiles_',case_study_name,'/',list_idParcel[i],'/parF', list_idParcel[i],'.csv'), header = TRUE,sep=",",dec = ".",stringsAsFactor=FALSE)
-#
-#  ####### 4.2 Create frame with all parameters #######
-#  param_frame <- rbind(param_frame, param)
-#
-#  ####### 4.3 Create frame with all irrigation vectors  #######
-# I1   = as.vector(meteo$day) ; I1[] = 0; I2 = I1 # I1 is surface irrigation and I2  I2 is deep buried irrigation (I2 is null)
-# irr = rbind(irr,I1)
-#}
+  #param_frame <- data.frame(); irr <- data.frame()
+  #for (i in 1:length(list_idParcel)){
+  #  ####### 4.1 Load params of each plot #######
+  #  param = read.csv(paste0(wd,'paramfiles/paramfiles_',case_study_name,'/',list_idParcel[i],'/parF', list_idParcel[i],'.csv'), header = TRUE,sep=",",dec = ".",stringsAsFactor=FALSE)
+  #
+  #  ####### 4.2 Create frame with all parameters #######
+  #  param_frame <- rbind(param_frame, param)
+  #
+  #  ####### 4.3 Create frame with all irrigation vectors  #######
+  # I1   = as.vector(meteo$day) ; I1[] = 0; I2 = I1 # I1 is surface irrigation and I2  I2 is deep buried irrigation (I2 is null)
+  # irr = rbind(irr,I1)
+  #}
 }
 
 
@@ -122,7 +125,7 @@ if (with_optirrig) {
 
 ####### 5.1 Create results dataFrame #######
 crop_results <- data.frame(idParcel = NULL, wsi = NULL, lai = NULL, hi = NULL, cropMaturitySignal = NULL)
-farmers_results<- data.frame(id = NULL, day = NULL, nbFloodPlotAffToday = NULL, dosCounter = NULL)
+farmers_results <- data.frame(id = NULL, day = NULL, nbFloodPlotAffToday = NULL, dosCounter = NULL)
 
 
 ####### 5.2 Run J2K from 1 DOY to DOY 120 (1er mai) to simulate the new state of the watershed before the irrigation campaign #######  #######
@@ -170,78 +173,107 @@ if (with_optirrig) {
 }
 
 ####### 5.4 Run WatASit-Optirrig coupled simulation from DOY 121 (1er mai) during the irrigation campaign #######
-for (day in cormas_doy_start:(cormas_doy_start + cormas_sim_day_nb)){
-      ####### 5.4.1 Update Cormas Meteo #######
-      P<-meteo$P; setAttributesOfEntities("p", "Meteo", 1, P[day]) # Precipitation conditions of the day
-      p_forecast = sum(c(P[day],P[day+1],P[day+2]), na.rm = TRUE); if (p_forecast > 0) {p_forecast = 1}; setAttributesOfEntities("p_forecast", "Meteo", 1, p_forecast) # Precipitation forecast for the next 3 days
-      # setAttributesOfEntities("p_forecast", "Meteo", 1, 1)
-      # if (day == 1) {p_cumTenDays = 0}
-      # if (day == 2) {p_cumTenDays = P[day-1]}
-      # if (day == 3) {p_cumTenDays = sum(c(P[day-1],P[day-2]), na.rm = TRUE)}
-      # if (day == 4) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3]), na.rm = TRUE)}
-      # if (day == 5) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4]), na.rm = TRUE)}
-      # if (day == 6) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5]), na.rm = TRUE)}
-      # if (day == 7) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6]), na.rm = TRUE)}
-      # if (day == 8) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6], P[day-7]), na.rm = TRUE)}
-      # if (day == 9) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6], P[day-7], P[day-8]), na.rm = TRUE)}
-      # if (day == 10) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6], P[day-7], P[day-8], P[day-9]), na.rm = TRUE)}
-      if (day >= 11) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6], P[day-7], P[day-8], P[day-9], P[day-10]), na.rm = TRUE)}
-      setAttributesOfEntities("p_cumTenDays", "Meteo", 1, p_cumTenDays) # Calculate cumulative precipitation for the last 10 days
-      # setAttributesOfEntities("p_cumTenDays", "Meteo", 1, 10)
-      # if (day <= 11) {p_cumFifteenDays = p_cumTenDays}
-      # if (day == 12) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11]), na.rm = TRUE)}
-      # if (day == 13) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11], P[day-12]), na.rm = TRUE)}
-      # if (day == 14) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11], P[day-12], P[day-13]), na.rm = TRUE)}
-      # if (day == 15) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11], P[day-12], P[day-13], P[day-14]), na.rm = TRUE)}
-      if (day >= 16) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11], P[day-12], P[day-13], P[day-14], P[day-15]), na.rm = TRUE)} else
-      {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-1], P[day-2], P[day-3], P[day-4], P[day-5]), na.rm = TRUE)}
-      setAttributesOfEntities("p_cumFifteenDays", "Meteo", 1, p_cumFifteenDays) # Calculate cumulative precipitation for the last 15 days
-      # setAttributesOfEntities("p_cumFifteenDays", "Meteo", 1, 50)
+for (day in cormas_doy_start:(cormas_doy_start + cormas_sim_day_nb)) {
+  ####### 5.4.1 Update Cormas Meteo #######
+  P <- meteo$P
+  setAttributesOfEntities("p", "Meteo", 1, P[day]) # Precipitation conditions of the day
+  p_forecast = sum(c(P[day], P[day+1], P[day+2]), na.rm = TRUE)
+  if (p_forecast > 0) {
+    p_forecast = 1
+  }
+  setAttributesOfEntities("p_forecast", "Meteo", 1, p_forecast) # Precipitation forecast for the next 3 days
+  # setAttributesOfEntities("p_forecast", "Meteo", 1, 1)
+  # if (day == 1) {p_cumTenDays = 0}
+  # if (day == 2) {p_cumTenDays = P[day-1]}
+  # if (day == 3) {p_cumTenDays = sum(c(P[day-1],P[day-2]), na.rm = TRUE)}
+  # if (day == 4) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3]), na.rm = TRUE)}
+  # if (day == 5) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4]), na.rm = TRUE)}
+  # if (day == 6) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5]), na.rm = TRUE)}
+  # if (day == 7) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6]), na.rm = TRUE)}
+  # if (day == 8) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6], P[day-7]), na.rm = TRUE)}
+  # if (day == 9) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6], P[day-7], P[day-8]), na.rm = TRUE)}
+  # if (day == 10) {p_cumTenDays = sum(c(P[day-1],P[day-2],P[day-3], P[day-4], P[day-5], P[day-6], P[day-7], P[day-8], P[day-9]), na.rm = TRUE)}
+  if (day >= 11) {
+    p_cumTenDays = sum(c(P[day-1], P[day-2], P[day-3], P[day-4], P[day-5], P[day-6], P[day-7], P[day-8], P[day-9], P[day-10]), na.rm = TRUE)
+  }
+  setAttributesOfEntities("p_cumTenDays", "Meteo", 1, p_cumTenDays) # Calculate cumulative precipitation for the last 10 days
+  # setAttributesOfEntities("p_cumTenDays", "Meteo", 1, 10)
+  # if (day <= 11) {p_cumFifteenDays = p_cumTenDays}
+  # if (day == 12) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11]), na.rm = TRUE)}
+  # if (day == 13) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11], P[day-12]), na.rm = TRUE)}
+  # if (day == 14) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11], P[day-12], P[day-13]), na.rm = TRUE)}
+  # if (day == 15) {p_cumFifteenDays = sum(c(p_cumTenDays,P[day-11], P[day-12], P[day-13], P[day-14]), na.rm = TRUE)}
+  if (day >= 16) {
+    p_cumFifteenDays = sum(c(p_cumTenDays, P[day-11], P[day-12], P[day-13], P[day-14], P[day-15]), na.rm = TRUE)
+  }
+  else {
+    p_cumFifteenDays = sum(c(p_cumTenDays, P[day-1], P[day-2], P[day-3], P[day-4], P[day-5]), na.rm = TRUE)
+  }
+  setAttributesOfEntities("p_cumFifteenDays", "Meteo", 1, p_cumFifteenDays) # Calculate cumulative precipitation for the last 15 days
+  # setAttributesOfEntities("p_cumFifteenDays", "Meteo", 1, 50)
 
-    ####### 5.4.2 Run coupled simulation of 24 hours #######
-     r <- runSimu(duration = 24)
-     response <- gettext(r[[2]])
-     if (response != "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"urn:vwservices\"><SOAP-ENV:Body><ns:RunSimuResponse><ns:result>true</ns:result></ns:RunSimuResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>") {stop("RUN STOPPED",call.=FALSE)} # To check if runSimu is done
-     obs1 <- getAttributesOfEntities("floodAffCounter","Efarmer")
-     obs2 <- getAttributesOfEntities("floodActCounter","Efarmer")
-     obs <- left_join(obs1,obs2, by = "id")
-     obs$day = day
-     farmers_results <- farmers_results %>%
-       bind_rows(obs)
+  ####### 5.4.2 Run coupled simulation of 24 hours #######
+  r <- runSimu(duration = 24)
+  response <- gettext(r[[2]])
+  # To check if runSimu is done
+  if (response != "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:ns=\"urn:vwservices\"><SOAP-ENV:Body><ns:RunSimuResponse><ns:result>true</ns:result></ns:RunSimuResponse></SOAP-ENV:Body></SOAP-ENV:Envelope>") {
+    stop("RUN STOPPED", call.=FALSE)
+  }
+  obs1 <- getAttributesOfEntities("floodAffCounter", "Efarmer")
+  obs2 <- getAttributesOfEntities("floodActCounter", "Efarmer")
+  obs <- left_join(obs1, obs2, by = "id")
+  obs$day = day
+  farmers_results <- farmers_results %>%
+    bind_rows(obs)
 
-      ####### 5.4.3 Get the state of crops from Cormas #######
-      idParcel      <- getAttributesOfEntities("idParcel", "Ecrop");  list_idParcel <- idParcel$idParcel
-      harvestSignal <- getAttributesOfEntities("harvestSignal", "Ecrop")
-      irriDailyDose <- getAttributesOfEntities("irriDailyDose", "Ecrop")
+  ####### 5.4.3 Get the state of crops from Cormas #######
+  idParcel      <- getAttributesOfEntities("idParcel", "Ecrop")
+  list_idParcel <- idParcel$idParcel
+  harvestSignal <- getAttributesOfEntities("harvestSignal", "Ecrop")
+  irriDailyDose <- getAttributesOfEntities("irriDailyDose", "Ecrop")
 
-      ####### 5.4.4 Simulate the new state of crops with Optirrig #######
-      if (with_optirrig) {
-                if (day != 1) { inval2_list <- list(); vect2_list <- list()
-                  for (i in 1:length(list_idParcel)){
-                    cat("Simulation of day",day, "and parcel number",i,"(idParcel =",list_idParcel[i],")","\n")
-                    irr[i,day]  <- irriDailyDose$irriDailyDose[i]; irr<-as.matrix(irr); I1 =irr[i,] # Update irrigation from Cormas
-                    param<-param_frame[i,]; cstes<-cstes_list[i,]; inval<-inval_list[i,]; vect<-vect_list[i,]
-                    optirday = daily_optirr(param,
-                                            meteo,
-                                            cstes,
-                                            inval,
-                                            vect,
-                                            I1, # Surface irrigation
-                                            I2, # Deep irrigation (buried drip)
-                                            day) # Time step
-                    inval2 = optirday$inval; inval2_list <- rbind(inval2_list, inval2);  inval_list[i,] <- inval2 # News constants
-                    vect2  = optirday$vect; vect2_list <- rbind(vect2_list, vect2); vect_list[i,] <- vect2 # New vectors
-                  }
-                }
+  ####### 5.4.4 Simulate the new state of crops with Optirrig #######
+  if (with_optirrig) {
+    if (day != 1) {
+      inval2_list <- list()
+      vect2_list <- list()
+      for (i in 1:length(list_idParcel)) {
+        cat("Simulation of day", day, "and parcel number", i, "(idParcel =", list_idParcel[i], ")", "\n")
+        irr[i,day]  <- irriDailyDose$irriDailyDose[i]
+        irr <- as.matrix(irr)
+        I1 = irr[i,] # Update irrigation from Cormas
+        param <- param_frame[i,]
+        cstes <- cstes_list[i,]
+        inval <- inval_list[i,]
+        vect <- vect_list[i,]
+        optirday = daily_optirr(
+          param,
+          meteo,
+          cstes,
+          inval,
+          vect,
+          I1, # Surface irrigation
+          I2, # Deep irrigation (buried drip)
+          day # Time step
+        )
+        inval2 = optirday$inval
+        inval2_list <- rbind(inval2_list, inval2)
+        inval_list[i,] <- inval2 # News constants
+        vect2  = optirday$vect
+        vect2_list <- rbind(vect2_list, vect2)
+        vect_list[i,] <- vect2 # New vectors
       }
-        ####### 5.4.5 Set the irrigation in J2K #######
-        j2kSet("drip", c(1,2,3), c(100, 100, 100)) # Mais en utilisant en fait les irriDailyDose ou truc du genre
-                                                  # récupérés ci-dessus depuis cormas
-      #TODO La ligne précédente renvoie une erreur chez moi, c'est pour ça que je l'ai commenté..:
+    }
+  }
+  ####### 5.4.5 Set the irrigation in J2K #######
+  j2kSet("drip", c(1,2,3), c(100, 100, 100)) # Mais en utilisant en fait les irriDailyDose ou truc du genre
+                                              # récupérés ci-dessus depuis cormas
+  #TODO La ligne précédente renvoie une erreur chez moi, c'est pour ça que je l'ai commenté..:
+  # C'est bon ça roule maintenant
 
-        ####### 5.4.6 Simulate the new state of watershed with J2K #######
-        j2kMakeStep(1)
-        reachQTable = j2kGet("reach")
+  ####### 5.4.6 Simulate the new state of watershed with J2K #######
+  j2kMakeStep(1)
+  reachQTable = j2kGet("reach")
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
       # ATTENTION JE NE COMPRENDS PAS POURQUOI CETTE SECTION N'EST PAS
@@ -249,20 +281,20 @@ for (day in cormas_doy_start:(cormas_doy_start + cormas_sim_day_nb)){
   ####### 6.Get new crop state from Optirrig simulations #######
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   if (with_optirrig) {
-  ####### 6.1 Get new crop states #######
-  # newLAI<-vectList
-  # newCropState <- data.frame(list_idParcel, vect2Frame$wsi, vect2Frame$lai, vect2Frame$hi, vect2Frame$cropMaturitySignal) # Tableau final avec les  sorties Optirrig pour chaque idParcel
+    ####### 6.1 Get new crop states #######
+    # newLAI<-vectList
+    # newCropState <- data.frame(list_idParcel, vect2Frame$wsi, vect2Frame$lai, vect2Frame$hi, vect2Frame$cropMaturitySignal) # Tableau final avec les  sorties Optirrig pour chaque idParcel
 
-  ####### 6.2 Set the new state of crops in cormas #######
-  #setAttributesOfEntities("wsi", "Ecrop", idParcel$id, newCropState$wsi)    # Vérifier qu'on prend bien les bonnes parcelles!!!!
-  # setAttributesOfEntities("lai", "Ecrop", idParcel$id, newCropState$lai)
-  #setAttributesOfEntities("hi", "Ecrop", idParcel$id, newCropState$hi)
- # setAttributesOfEntities("cropMaturiySignal", "Ecrop", idParcel$id, newCropState$cropMaturitySignal)
+    ####### 6.2 Set the new state of crops in cormas #######
+    #setAttributesOfEntities("wsi", "Ecrop", idParcel$id, newCropState$wsi)    # Vérifier qu'on prend bien les bonnes parcelles!!!!
+    #setAttributesOfEntities("lai", "Ecrop", idParcel$id, newCropState$lai)
+    #setAttributesOfEntities("hi", "Ecrop", idParcel$id, newCropState$hi)
+    #setAttributesOfEntities("cropMaturiySignal", "Ecrop", idParcel$id, newCropState$cropMaturitySignal)
 
-  ####### 6.3 Save results in R (even though they are also in cormas) #######
-  # newCropState$irrigation <- irriDailyDose$values
-  # newCropState$day <- day
-  # cropResults <- rbind(cropResults, newCropState )
+    ####### 6.3 Save results in R (even though they are also in cormas) #######
+    # newCropState$irrigation <- irriDailyDose$values
+    # newCropState$day <- day
+    # cropResults <- rbind(cropResults, newCropState )
   }
 
 }
