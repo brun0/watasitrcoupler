2657542aba000665328a33787476a37dd56ed6fd
Julien Veyssier <eneiluj@posteo.net>
2020-04-08 16:33:47 +0200

    attemp to close visualworks nicely with closeVisualWorks() does not work
    Signed-off-by: Julien Veyssier <eneiluj@posteo.net>

--- Rcoupler.R
+++ Rcoupler.R
@@ -406,6 +406,9 @@ j2kStop()
 Sys.sleep(5)
 killJ2K()
 
+# ça ne marche pas
+#closeVisualWorks()
+# le problème avec tuer visualworks est que ça met ensuite environ une minute à libérer le port 4920
 system2('kill', args=c('-9', "$(ps aux | grep -i cormas | grep visual | awk '{print $2}')"), stdout=stdoutP, stderr=stderrP)
 
 # Setting meteo if starting day of Cormas is lower than 10:
