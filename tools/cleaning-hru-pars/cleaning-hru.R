cae9b96880e5b0c6ef5aa361e13a47ec35e7b407
Bruno Bonte <bonte.bruno@gmail.com>
2021-04-27 12:26:23 +0200

    Removing Hrus with small area in GrandBuech jams model
    	* Updating cowat_for_new_com_module_GB.jam with new hru_cowat_minarea_grand_buech.par file
    	* removing hrus tronquee with null area (checked that there is no upstream hrus)
    	* removing hrus-plot with area inferior than 10 m2

---
+++ tools/cleaning-hru-pars/cleaning-hru.R
@@ -0,0 +1,22 @@
+library(igraph)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(stringr)
+
+hrus <- read.table("superjams/data/J2K_cowat/parameter/hru_cowat_10_cor_grand_buech.par", 
+                   skip = 5,
+                   sep= "\t",
+                   dec = ".") %>%
+  tbl_df()
+#nb correspondance collonnes: V1, V2, V6, V7, V9,    V13,    V14
+                              #id,area,x,y,subbassin,to_poly,to_reach
+
+valide_hrus <- hrus%>% filter(V2 > 10) %>% tbl_df()
+
+write.table(valide_hrus,
+            "hru_cowat_withplots.par.dat",
+            sep = "\t",
+            dec= ".",
+            row.names = F)
+
